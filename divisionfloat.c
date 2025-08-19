8a788c56dc51392b5b8fd25e0ce0b1d9d9c3fe87
priyanshuborad007-creator <priyanshuborad007@gmail.com>
2025-08-19 17:41:33 +0530

    Add files via upload

---
+++ divisionfloat.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+float main(){
+float i,j,c;
+printf("enter the numbers: ");
+scanf("%f %f",&i,&j);
+c=i/j;
+printf("%f / %f = %f",c);
+return 0;
+}
---
+++ substraction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int i,j,c;
+printf("enter the numbers: ");
+scanf("%d %d",&i,&j);
+c=i-j;
+printf("%d - %d = %d",c);
+return 0;
+}
